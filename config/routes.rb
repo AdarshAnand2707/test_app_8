6a07b3d84d52d46d4049a95dc0e66022097513ea
AdarshAnand2707 <adarshanand456@gamil.com>
2025-03-22 21:35:28 +0530

    Fixing String Quotes

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  root 'pages#home'
-  get 'about', to: 'pages#about'
+  root "pages#home"
+  get "about", to: "pages#about"
 end
